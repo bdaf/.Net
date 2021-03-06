953bb4d070cee5705ff779d230a0ae532e9f17b1
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-03-06 11:34:40 +0100

    Created 'Hello World' program

---
+++ FizzBuzz_Console/FizzBuzz_Console/FizzBuzz_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzz_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
