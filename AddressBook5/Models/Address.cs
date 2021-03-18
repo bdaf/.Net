5ac5ec942a9e567a3b455258d935e26c62e64380
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-03-18 15:53:09 +0100

    final restoring files

---
+++ AddressBook5/Models/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook5.Models {
+    public class Address {
+        [Display(Name = "My favourite street"), Required(ErrorMessage = "Please provide a valid street.")]
+        public string Street { get; set; }
+        [Required(ErrorMessage = "Please provide a number too.")]
+        public string Number { get; set; }
+        [Display(Name = "My ZipCode!"), Required(ErrorMessage = "Yes, provide ZipCode either. ")]
+        public string ZipCode { get; set; }
+        [Display(Name = "My Big (or not) City!")]
+        [StringLength(60,MinimumLength = 3), Required(ErrorMessage = "Even if it's small, provide it!")]
+        public string City { get; set; }
+    }
+}
---
+++ AddressBook5/Pages/Privacy.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook5.Pages {
+    public class PrivacyModel : PageModel {
+        private readonly ILogger<PrivacyModel> _logger;
+        [BindProperty(SupportsGet = true)]
+        public string Message { get; set; }
+
+        public PrivacyModel(ILogger<PrivacyModel> logger) {
+            _logger = logger;
+        }
+
+        public void OnGet() {
+            if(string.IsNullOrWhiteSpace(Message))
+                Message = "*use '/?message=' and change this string*";
+        }
+    }
+}
