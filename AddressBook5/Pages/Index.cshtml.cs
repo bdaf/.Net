edd4093b6dbc119d584e2be70d69482ba2ac3b6a
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-03-18 15:47:16 +0100

    restoring files

---
+++ AddressBook5/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using AddressBook5.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook5.Pages {
+    public class IndexModel : PageModel {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public Address Address { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        
+
+        public IndexModel(ILogger<IndexModel> logger) {
+            _logger = logger;
+        }
+
+        public void OnGet() {
+            if(string.IsNullOrWhiteSpace(Name)) {
+                Name = "Default User";
+            }
+        }
+
+        public IActionResult OnPost() {
+            if(ModelState.IsValid)
+                return RedirectToPage("./Privacy");
+            return Page();
+        }
+    }
+}
