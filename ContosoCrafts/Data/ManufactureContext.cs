5a857f5beb3677038abc068b2575f937b87a9e6d
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-04-13 21:03:42 +0200

    Records from database and records from file works properly

---
+++ ContosoCrafts/Data/ManufactureContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoCrafts.Data {
+    public class ManufactureContext : DbContext {
+        public DbSet<Product> Product { get; set; }
+
+        public ManufactureContext(DbContextOptions options) : base(options) {
+        }
+    }
+}
--- ContosoCrafts/Startup.cs
+++ ContosoCrafts/Startup.cs
@@ -1,7 +1,9 @@
+using ContosoCrafts.Data;
 using ContosoCrafts.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,6 +24,9 @@ namespace ContosoCrafts {
         public void ConfigureServices(IServiceCollection services) {
             services.AddRazorPages();
             services.AddTransient<JsonFileProductService>();
+            services.AddDbContext<ManufactureContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("ContosoCraftsDB"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
