8bbdb471f7dd3cb3f118a08558e70e6179089d8c
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-04-26 21:02:26 +0200

    sending confirmation emails works

--- ClaimsIdentity/Services/AuthMessageSenderOptions.cs
+++ ClaimsIdentity/Services/AuthMessageSenderOptions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ClaimsIdentity.Services {
-    public class AuthMessageSenderOptions {
-    }
+public class AuthMessageSenderOptions {
+    public string SendGridUser { get; set; }
+    public string SendGridKey { get; set; }
 }
