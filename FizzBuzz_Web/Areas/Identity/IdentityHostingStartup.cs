14838d4cecd893324f1108ca7b402d742e47f29e
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-04-27 13:20:01 +0200

    Login and Register page appear in FizzBuzz_Web

---
+++ FizzBuzz_Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FizzBuzz_Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FizzBuzz_Web.Areas.Identity.IdentityHostingStartup))]
+namespace FizzBuzz_Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FizzBuzz_WebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FizzBuzz_WebContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<FizzBuzz_WebContext>();
+            });
+        }
+    }
+}
